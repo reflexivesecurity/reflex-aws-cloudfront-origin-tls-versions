2efed760c283ad28045916dc307db1ed90638b35
Chris Coleman <chris@rot-26.com>
2020-03-21 23:36:57 -0600

    init with template

---
+++ reflex_aws_cloudfront_origin_tls_versions.tf
@@ -0,0 +1,31 @@
+module "reflex_aws_cloudfront_origin_tls_versions" {
+  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.4"
+  rule_name        = "CloudfrontOriginTlsVersions"
+  rule_description = "TODO: Provide rule description"
+
+  event_pattern = <<PATTERN
+# TODO: Provide event pattern
+PATTERN
+
+  function_name   = "CloudfrontOriginTlsVersions"
+  source_code_dir = "${path.module}/source"
+  handler         = "reflex_aws_cloudfront_origin_tls_versions.lambda_handler"
+  lambda_runtime  = "python3.7"
+  environment_variable_map = {
+    SNS_TOPIC = var.sns_topic_arn,
+    MODE      = var.mode
+  }
+  custom_lambda_policy = <<EOF
+# TODO: Provide required lambda permissions policy
+EOF
+
+
+
+  queue_name    = "CloudfrontOriginTlsVersions"
+  delay_seconds = 0
+
+  target_id = "CloudfrontOriginTlsVersions"
+
+  sns_topic_arn  = var.sns_topic_arn
+  sqs_kms_key_id = var.reflex_kms_key_id
+}

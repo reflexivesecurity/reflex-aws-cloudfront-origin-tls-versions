29a84dc22b6ed148ad88a77b2e5fd586e308f173
Chris Coleman <chris@rot-26.com>
2020-03-22 19:56:32 -0600

    remove unused mode variable (for now)

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "sns_topic_arn" { 
+variable "sns_topic_arn" {
   description = "SNS topic arn of central or local sns topic"
   type        = string
 }
@@ -6,10 +6,4 @@ variable "sns_topic_arn" {
 variable "reflex_kms_key_id" {
   description = "KMS Key Id for common reflex usage."
   type        = string
-}
-
-variable "mode" {
-  description = "The mode that the Rule will operate in. Valid choices: DETECT | REMEDIATE"
-  type        = string
-  default     = "detect"
 }
